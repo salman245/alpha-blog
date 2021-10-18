62336bd3cfca88cf452d43e74e827f7953d17e0f
Salman Tahir <salmantahir3249@gmail.com>
2021-10-18 14:46:05 +0500

    All Crud operations are done

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root 'page#home'
 
   get 'about', to: "page#about"
-  resources :articles, only: [:show, :index, :new, :create]
+  resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
