da7c7d4fa8e0a4d9ca17b4c3cfb2a3e4b1a6a4e7
Salman Tahir <salmantahir3249@gmail.com>
2021-10-20 17:36:00 +0500

    Crud Operations - done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,9 @@
 class ArticlesController < ApplicationController
 
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
 
     def show
-        @article =  Article.find(params[:id])
+      
 
     end
 
@@ -15,11 +16,10 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-    @article =  Article.find(params[:id])
     end 
 
     def create
-        @article  =  Article.new(params.require(:article).permit(:title, :description))
+        @article  =  Article.new(article_params)
         
         if @article.save
             flash[:notice] = "Article was created Successfully"
@@ -31,8 +31,7 @@ class ArticlesController < ApplicationController
     end
 
     def update
-        @article =  Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title, :description))
+        if @article.update(article_params)
             flash[:notice] = "Article was updated Successfully"
             redirect_to @article
         else
@@ -42,9 +41,19 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
-        @article =  Article.find(params[:id])
         @article.destroy
         redirect_to articles_path
     end
+
+
+    private
+    def set_article
+        @article =  Article.find(params[:id])
+    end
+
+    def article_params 
+        params.require(:article).permit(:title, :description)
+    end
+
 
 end
