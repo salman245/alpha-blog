140cf56df2fe55013548215e516ed91583a8150f
Salman Tahir <salmantahir3249@gmail.com>
2021-10-27 17:18:33 +0500

    added Create user action

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
 
   get 'about', to: "page#about"
   resources :articles
+
+  get 'signup', to:"users#new"
+  resources :users, except: [:new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
