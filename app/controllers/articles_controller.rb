3ddacba5a9b7bc8cebf6a72145366f460002dde4
Salman Tahir <salmantahir3249@gmail.com>
2021-10-27 14:26:26 +0500

    One to many association between user and articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,7 +20,7 @@ class ArticlesController < ApplicationController
 
     def create
         @article  =  Article.new(article_params)
-        
+        @article.user = User.first 
         if @article.save
             flash[:notice] = "Article was created Successfully"
            redirect_to @article
