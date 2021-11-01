6d29361d5c5e1f32644b213838b2c2357d6d66a8
Salman Tahir <salmantahir3249@gmail.com>
2021-11-01 17:55:48 +0500

    Delete User

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,8 @@ class ArticlesController < ApplicationController
 
     before_action :set_article, only: [:show, :edit, :update, :destroy]
 
+    before_action :require_user, except: [:show, :index]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
     def show
       
 
@@ -21,7 +23,7 @@ class ArticlesController < ApplicationController
 
     def create
         @article  =  Article.new(article_params)
-        @article.user = User.first 
+        @article.user = current_user 
         if @article.save
             flash[:notice] = "Article was created Successfully"
            redirect_to @article
@@ -56,5 +58,12 @@ class ArticlesController < ApplicationController
         params.require(:article).permit(:title, :description)
     end
 
+    def require_same_user
+        if current_user !=  @article.user
+            flash[:alert] =  "You can only edit or destroy your own article"
+            redirect_to @article
+        end
+
+    end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
 class UsersController < ApplicationController
+before_action  :set_user, only: [:show, :edit, :update, :destroy]
+before_action :require_user, only: [:edit, :update]
 
+before_action :require_same_user, only: [:edit, :update, :destroy]
     def index
         @users =  User.paginate(page: params[:page], per_page: 4)
     end
 
     def show
-        @user =  User.find(params[:id])
+ 
         @articles = @user.articles.paginate(page: params[:page], per_page: 4)
     end
 
@@ -14,11 +17,11 @@ class UsersController < ApplicationController
     end
        
     def edit
-        @user =  User.find(params[:id])
+      
     end
 
     def update
-        @user =  User.find(params[:id])
+      
         if @user.update(user_params)
             flash[:notice] = "User profile is updated Successfully"
             redirect_to @user
@@ -32,6 +35,7 @@ class UsersController < ApplicationController
     def create
         @user =  User.new(user_params)
         if @user.save
+            session[:user_id] =  @user.id
             flash[:notice]  = "Welcome to Alpha Blog #{@user.username}, you have Successfully Signed Up"
             redirect_to articles_path
         else
@@ -39,6 +43,12 @@ class UsersController < ApplicationController
         end
     end
 
+    def destroy
+        @user.destroy
+        session[:user_id] = nil
+        flash[:notice] = "Account and all related articles are deleted"
+        redirect_to articles_path
+    end
 
 
 
@@ -52,4 +62,17 @@ class UsersController < ApplicationController
     def user_params
         params.require(:user).permit(:username, :email, :password)
     end
+
+    def set_user
+        @user =  User.find(params[:id])
+    end
+
+    def require_same_user
+        if current_user != @user
+            flash[:alert] =  "You can only edit your own profile"
+            redirect_to @user
+        end
+
+    end
+
 end
