45916131c90563cfb8e4739ca45369b1903e1a77
Salman Tahir <salmantahir3249@gmail.com>
2021-11-01 11:59:20 +0500

    Added Pagination

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,8 @@ class ArticlesController < ApplicationController
     end
 
     def index
-        @articles =  Article.all
+        @articles =  Article.paginate(page: params[:page], per_page: 4)
+
     end
 
     def new
