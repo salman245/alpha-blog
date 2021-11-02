5945ca5d631026a9b0433502483ccbe042eea467
Salman Tahir <salmantahir3249@gmail.com>
2021-11-02 11:49:56 +0500

    Section 6 is done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,7 +45,7 @@ before_action :require_same_user, only: [:edit, :update, :destroy]
 
     def destroy
         @user.destroy
-        session[:user_id] = nil
+        session[:user_id] = nil if @user == current_user 
         flash[:notice] = "Account and all related articles are deleted"
         redirect_to articles_path
     end
@@ -68,8 +68,8 @@ before_action :require_same_user, only: [:edit, :update, :destroy]
     end
 
     def require_same_user
-        if current_user != @user
-            flash[:alert] =  "You can only edit your own profile"
+        if current_user != @user  && !current_user.admin?
+            flash[:alert] =  "You can only edit or delete your own profile"
             redirect_to @user
         end
 
